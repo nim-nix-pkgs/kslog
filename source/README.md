7ffcff9e4b2f9d286132cbf8a86049d21c77a21d
hugosenari <hugosenari@gmail.com>
2022-09-03 19:14:31 -0300

    chore: re index kslog

---
+++ source/README.md
@@ -0,0 +1,67 @@
+Privilege and Complexity
+========================
+I mostly wrote this because I wanted to run my kernel/system logger not as
+root.  I looked into doing this with `syslog-ng` and it seemed hard to get
+right.  `CAP_SYS_ADMIN` or whatnot also seem used.  I had a hunch that over
+the years feature bloat had exploded sysloggers beyond reason making what I
+wanted unnecessarily difficult.  For what most people use it for, it should
+really be a simple program anyway.  syslog-ng is over 300,000 lines of C.
+Even busybox syslogd clocks in at over 1,000 lines.
+
+Instead of all that jazz, I give you `kslog` - under 200 lines of Nim that
+likely does all you really need in two easy pieces - a few dozen line easily
+audited privileged `kslog_open.nim` and 125-ish line `kslog.nim`.
+
+Sadly, `kslog-open` (*not* `kslog`) must run as root to manipulate `/dev/`.
+At this late date, there is probably no relocating of `/dev/log` or making
+binding of Unix domain sockets easier.  This opening phase is *all* `kslog`
+needs elevated privilege for.  `kslog-open` just does this minimal work to
+set up input file descriptors 0,3 and then drops privilege & exec's `kslog`.
+`kslog` itself only needs permission to open its output files for write.
+If said output files already exist with `syslog`-user writable permission,
+the `syslog` user need not even have permission to create new files in
+`/var/log`.  Wide ability to write to `/dev/log` always affords an easy
+fill-the-disk attack, of course.
+
+Log Contents
+=============
+Priority & facility numbers are retained in `kslog` logs.  I doubt there is a
+better way to learn how to filter out informational|debug messages by altering
+`maxLevel` than looking at a big list of examples.  `grep '\<P[67],F[0-9]'
+/var/log/msgs` does just that.  Personally, I keep all priority levels, but
+PF-retention also makes it easy to grep for important things, too, while also
+keeping the convenience of just one file.  Dropping these fields (and calendar
+years!) harkens to disk space concerns long since past.
+
+Log Rotation
+============
+Personally, I only do this every several years or so, but if disk space in
+`/var/log/` is at a premium (a bad idea, but sometimes things happen), you
+can still rotate logs.  Since `kslog` never re-opens output files, showing
+how to do this reliably here is warranted.  Would be external log rotators
+should SIGSTOP `kslog`, copy files, then truncate logs to zero, then SIGCONT.
+To avoid losing msgs from filled backlogs, care should be taken to not leave
+`kslog` suspended indefinitely or even very long.  An example shell script is
+`log-arch` using the also included `cp_trunc.nim` program which usually only
+has to suspend kslog for "around milliseconds".  Considering times are only
+1-second resolution, it is doubtful that delay would ever matter.
+
+Remote Logs
+===========
+When you want remote logs on some more trusted machine then I recommend
+providing remote `rsync` access to local logs made by `kslog`.  Provided
+this access is one-way (trusted can access `kslogs` but not vice versa),
+I'd think this adequate protection/detection from intruders altering logs.
+It is much lower tech just using ssh/rsync/etc. which you likely already
+know how to use and additionally supports logs updated by entities other
+than syslog (e.g. `wtmp`).  Detecting even transient revocation of such
+remote access by a local intruder is also easy.  This idea does not solve
+the problem of literally zero local space for logs, though a network FS
+may (while still copying from less to more trusted domains).
+
+Efficiency
+==========
+Compiled Nim programs are efficient.  I routinely see CPU usage in the range
+50..400 parts per ***billion*** with an RSS under 2 MiB.  This obviously varies
+based on how much your system logs.  In any event, while I have not measured it
+I strongly suspect this program uses much less space/time than, say, syslog-ng.
---
+++ source/cp_trunc.nim
@@ -0,0 +1,92 @@
+import os, posix, times, strutils, cligen
+var buffer = newStringOfCap(16000)
+var st: Stat
+let buf = buffer[0].addr.pointer
+let siz = 16000
+
+proc isSuspended(pid: Pid=0, delayUsec=40): bool =
+  if pid == 0:                              #No delay and/or pid does not exist
+    return true
+  if stat("/proc/stat", st) == 0:
+    let path = "/proc/" & $pid & "/status"
+    let pfd = open(path.cstring, O_RDONLY)
+    if pfd == -1:   #Allowed to to signal or cp_trunc would already have failed
+      return true   #So, -1 here can only mean pid failed to exist => proceed
+    let nR = read(pfd, buf, siz)
+    discard close(pfd)
+    if nR <= 0:                             #Likely impossible once open works
+      return true                           #Assume PID must be dead/zombie
+    buffer.setLen nR                        #Set length of Nim string to amt rd
+    let rightParen = buffer.rfind(')')      #Status is 1-char field post (cmd)
+    if rightParen < buffer.len - 2:         #Also probably impossible
+      return true                           #Assume PID must be dead/zombie
+    result = buffer[rightParen + 1] == 'T'
+    if not result:
+      discard usleep(delayUsec.Useconds)
+  else:                                     #/proc/stat not present
+    discard usleep(2000)                    #  => Just sleep 2ms
+    return true                             #..and always return true
+
+proc cp_trunc*(src: string, dst: string, pid: Pid=0, verbose=false): int =
+  ## ``cp_trunc`` copies ``src`` to ``dst`` (both paths to regular files),
+  ## SIGSTOPs ``pid`` to ensure it stops appending, copies any new data,
+  ## truncates ``src`` to 0 and SIGCONTs ``pid``.  This is nice for rotating a
+  ## log file being actively appended to by a known ``pid`` (via O_APPEND).
+  ## Simpler approaches cannot ensure that no data is missed (from the brief
+  ## time between the end of the first copy and truncation).  Zero return
+  ## means everything went as planned.
+
+  template fail(msg: string, eno: cint, rv: int) =
+    stderr.write msg & ": " & $strerror(eno) & "\n"
+    return rv
+
+  template resume_fail(pid: Pid, msg: string, eno: cint, rv: int) =
+    let e = eno
+    if pid != 0: discard kill(pid, SIGCONT)
+    fail msg, e, rv
+
+  if pid != 0 and kill(pid, 0) == -1 and errno == EPERM:  #signal perm check
+    fail "cannot signal " & $pid, errno, 2  #(Also proceed if pid doesn't exist)
+  let sfd = open(src, O_RDWR)               #Source read-write (since we trunc)
+  if sfd == -1: fail "open(\"" & src & "\")", errno, 3
+  defer: discard close(sfd)
+  discard fstat(sfd, st)
+  if not S_ISREG(st.st_mode): fail src & " is not a regular file", 0, 4
+  let dfd = open(dst, O_CREAT or O_TRUNC or O_WRONLY, 0o666) #Make Destination
+  if dfd == -1: fail "open(\"" & dst & "\")", errno, 5
+  defer: discard close(dfd)
+  discard fstat(dfd, st)
+  if not S_ISREG(st.st_mode): fail dst & " is not a regular file", 0, 6
+  if verbose: echo "copying " & src & " -> " & dst
+  while true:                               #Copy bytes until we run out
+    let nR = read(sfd, buf, siz)
+    if nR > 0:
+      let nW = write(dfd, buf, nR)          #May run out of space for dst
+      if nW != nR: fail "write dst", errno, 7
+    if nR != siz: break
+  var t0: float                             #Maybe report pause time
+  if pid != 0:
+    if verbose:
+      echo "pausing " & $pid
+      t0 = epochTime()
+    discard kill(pid, SIGSTOP)   #Pause writer|proceed since writer must be gone
+    while not isSuspended(pid): discard     #Wait for de-scheduling of pid
+    while true:                             #Now copy any NEW data
+      let nR = read(sfd, buf, siz)          #This loop will almost always be
+      if nR > 0:                            #..just 1 read & 1 write, but we
+        let nW = write(dfd, buf, nR)        #..code it for rapid append cases.
+        if nW != nR: resume_fail pid, "write dst", errno, 8
+      if nR != siz: break
+  if ftruncate(sfd, 0) != 0:                #Copied all data: Truncate src!
+    resume_fail pid, "ftruncate", errno, 9
+  if pid != 0:
+    discard kill(pid, SIGCONT)              #Resume writer, now appending from 0
+    if verbose:
+      let dt = $(int((epochTime() - t0) * 1e6 + 0.5)) & " microseconds"
+      echo "truncated \"" & src & "\"; resumed " & $pid & " after " & dt
+
+dispatch(cp_trunc, cmdName="cp-trunc",
+         help = { "src"    : "source path to copy & truncate",
+                  "dst"    : "destination path of copy",
+                  "pid"    : "pid to pause during tail copy (0=>no pause)",
+                  "verbose": "print activity" })
